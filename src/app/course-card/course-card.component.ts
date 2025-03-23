1c322f998c8414874bf792b75f9910ed012b35e7
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-03-23 14:43:51 -0700

    Working html-card with bug fix
    Bug fix: added Angular Component to imports fields on `app.module.ts` file. HTML item card feature finally works now, given Angular 19  and Node v18 (outdated Node)

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  constructor() { }
+
+  // ngOnInit() {
+  // }
+  
+}
