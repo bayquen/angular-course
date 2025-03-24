71df1e314fecb306b2df09421007edf0ad8d4bc3
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-03-23 20:34:15 -0700

    @Input and @Output (Event Emitters)
    Angular Components: @Input and @Output decorators

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {COURSES} from '../../db-data'
+import {Course} from '../model/course';
 
 @Component({
   selector: 'course-card',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
-export class CourseCardComponent {
-  constructor() { }
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
 
-  // ngOnInit() {
-  // }
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
   
+  onCourseViewed() {
+
+    console.log("card component - button clicked ...");
+
+    this.courseEmitter.emit(this.course);
+  }
+
 }
