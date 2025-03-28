3c5a5b7adab25cf2aa6674ad663f18ecc1c0ef63
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-03-27 17:30:50 -0700

    More Angular Core directives practice

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {COURSES} from '../../db-data'
 import {Course} from '../model/course';
+import { CommonModule, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'course-card',
-  imports: [],
+  imports: [CommonModule, NgStyle],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
@@ -19,7 +20,6 @@ export class CourseCardComponent implements OnInit {
   // @Input({required: true})
   // index: number;
 
-
   @Output('courseSelected')
   courseEmitter = new EventEmitter<Course>();
 
@@ -29,11 +29,27 @@ export class CourseCardComponent implements OnInit {
   ngOnInit() {
   }
   
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
+
   onCourseViewed() {
 
     console.log("card component - button clicked ...");
 
     this.courseEmitter.emit(this.course);
+  }
+
+  cardClasses() {
+    if (this.course.category == 'BEGINNER') {
+      return 'beginner';
+    }
+  }
+
+  cardStyles() {
+    return {'background-image': 'url(' + this.course.iconUrl + ')'
+
+    };
   }
 
 }
